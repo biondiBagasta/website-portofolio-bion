02710cf97987cce5b9547e474f66d79de0c2bb4a
Biondi Bagasta Wiko Putra <siapaayo41@gmail.com>
2024-05-21 08:23:17 +0800

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { PersonalInfoComponent } from "./components/personal-info/personal-info.component";
+import { TopMenuComponent } from "./components/top-menu/top-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    PersonalInfoComponent,
+    TopMenuComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'my-portofolio-website';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "about",
+		loadComponent: () => import("./about/about.component").then((m) => m.AboutComponent)
+	},
+	{
+		path: "",
+		redirectTo: "about",
+		pathMatch: "full"
+	}
+];
